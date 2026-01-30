be5161ab177c0f0154faf925aeb48dc7f3051411
Evgniia <zeneka44@gmail.com>
2026-01-30 23:02:01 +0100

    add: actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/library-catalog-spa",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
